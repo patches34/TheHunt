d5fd6d8fb481772c7792ab2a660bf500993afbd6
Eric Baker <eric.baker@sharpnotions.com>
2017-08-30 17:27:45 -0400

    Created game manager

---
+++ Assets/Scripts/Singletons/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Analytics;
+using System.Collections;
+
+
+public enum TurnState
+{
+	Player,
+	Hunter,
+	Animal
+}
+
+public class GameManager : Singleton<GameManager>
+{
+	public TurnState turn {get; private set;}
+
+	#region Initialization
+	// Use this for initialization
+	protected GameManager()
+	{
+		// guarantee this will be always a singleton only - can't use the constructor!
+	}
+	#endregion
+}
