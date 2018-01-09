85b2f7809769da41cd1bb9ddc669e29f67e6c8b6
Eric Baker <eric.baker@sharpnotions.com>
2018-01-09 14:07:09 -0500

    Changed game over screen layout
    Game over screen now shows better text for the reason for the game over
    
    close #63

--- Assets/Scripts/UI/Menus/GameOverMenu.cs
+++ Assets/Scripts/UI/Menus/GameOverMenu.cs
@@ -5,7 +5,17 @@ using UnityEngine.UI;
 
 public class GameOverMenu : UIMenu
 {
-	public Text reasonLabel;
+	[SerializeField]
+	Text overLabel, reasonLabel;
+
+	[SerializeField]
+	GameObject restartObj;
+
+	const string k_PLAYER_WON = "Animal Save";
+	const string k_ANIMAL_STARVED = "Animal Died";
+	const string k_HUNTER_WON = "Animal Killed";
+	const string k_WON = "YOU WON!";
+	const string k_LOST = "GAME OVER";
 
 	// Use this for initialization
 	void Start () {
@@ -29,17 +39,28 @@ public class GameOverMenu : UIMenu
 
 	public override void SetVisible (bool value)
 	{
-		switch(GameManager.Instance.gameOverReason)
+		if(value)
 		{
-		case GameOverReason.PLAYER_WON:
-			reasonLabel.text = "Player Won!";
-			break;
-		case GameOverReason.ANIMAL_STARVED:
-			reasonLabel.text = "Animal Starved";
-			break;
-		case GameOverReason.HUNTER_WON:
-			reasonLabel.text = "Animal Killed";
-			break;
+			switch(GameManager.Instance.gameOverReason)
+			{
+			case GameOverReason.PLAYER_WON:
+				overLabel.text = k_WON;
+				reasonLabel.text = k_PLAYER_WON;
+				restartObj.SetActive(false);
+				break;
+			case GameOverReason.ANIMAL_STARVED:
+				overLabel.text = k_LOST;
+				reasonLabel.text = k_ANIMAL_STARVED;
+				break;
+			case GameOverReason.HUNTER_WON:
+				overLabel.text = k_LOST;
+				reasonLabel.text = k_HUNTER_WON;
+				break;
+			}
+		}
+		else
+		{
+			restartObj.SetActive(true);
 		}
 
 		base.SetVisible (value);
